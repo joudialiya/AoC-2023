c68635c71f43f7f94162d6b15d979b56a0241142
joudialiya <mlagzal.sayhi@gmail.com>
2023-12-09 21:33:41 +0000

    day8,9

---
+++ day9/puzzleB.py
@@ -0,0 +1,29 @@
+"""
+For the second part i will modify this function
+to return the next value on the left of the array
+"""
+def getNext(list: list):
+    allZero = True
+    for n in list:
+        if n != 0:
+            allZero = False
+            break
+    if allZero:
+        return 0
+    derived = []
+    for i in range(1, len(list)):
+        derived.append(list[i] - list[i - 1])
+
+    return list[0] - getNext(derived)
+        
+
+def solution():
+    with open("input", 'r') as file:
+        lines = file.read().splitlines()
+        values = [list(map(lambda n: int(n), line.split())) for line in lines]
+        sum = 0
+        for vector in values:
+           sum += getNext(vector)
+        return sum
+if __name__ == "__main__":
+    print(solution())
